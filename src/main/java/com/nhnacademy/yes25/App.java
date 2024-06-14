52a0ccf3a6cd111ebdee5956f9bb228f18b44b4a
채상희 <chaesanghui142@gmail.com>
2024-06-14 11:29:24 +0900

    add

---
+++ src/main/java/com/nhnacademy/yes25/App.java
@@ -0,0 +1,13 @@
+package com.nhnacademy.yes25;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
