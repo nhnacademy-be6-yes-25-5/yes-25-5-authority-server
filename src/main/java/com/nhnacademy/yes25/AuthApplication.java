fab12ff3fd8a4be49fb3789f31ffab754ffb6df3
채상희 <chaesanghui142@gmail.com>
2024-06-18 10:18:56 +0900

    feat: eurekaConfig 추가 후 ci, cd test 2

--- src/main/java/com/nhnacademy/yes25/AuthApplication.java
+++ src/main/java/com/nhnacademy/yes25/AuthApplication.java
@@ -2,11 +2,7 @@ package com.nhnacademy.yes25;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
-@EnableDiscoveryClient
-@ConditionalOnProperty(name = "eureka.client.enabled", havingValue = "true", matchIfMissing = true)
 @SpringBootApplication
 public class AuthApplication {
 
