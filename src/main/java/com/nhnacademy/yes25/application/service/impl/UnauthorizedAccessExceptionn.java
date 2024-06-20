845376f76f8416b9639e0a263b4d0f562fc5b183
채상희 <chaesanghui142@gmail.com>
2024-06-20 20:28:49 +0900

    fix: "ci 실패로 인한 ci.yml 파일 수정"

--- src/main/java/com/nhnacademy/yes25/application/service/impl/UnauthorizedAccessExceptionn.java
+++
@@ -1,7 +0,0 @@
-package com.nhnacademy.yes25.application.service.impl;
-
-public class UnauthorizedAccessExceptionn extends RuntimeException {
-    public UnauthorizedAccessExceptionn(String message) {
-        super(message);
-    }
-}
