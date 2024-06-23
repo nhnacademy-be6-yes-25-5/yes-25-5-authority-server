1529046bd8b97febd2b016331faf6d8196b34177
채상희 <chaesanghui142@gmail.com>
2024-06-23 17:33:00 +0900

    fix: "dev/prod 환경 설정"

--- src/main/java/com/nhnacademy/yes25/infrastructure/adaptor/UserAdaptor.java
+++ src/main/java/com/nhnacademy/yes25/infrastructure/adaptor/UserAdaptor.java
@@ -6,7 +6,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
-@FeignClient(name = "userAdaptor", url = "${eureka.client.service-url.defaultZone}")
+@FeignClient(name = "userAdaptor", url = "${eureka.gateway}/auth")
 public interface UserAdaptor {
 
     @PostMapping("/users")
