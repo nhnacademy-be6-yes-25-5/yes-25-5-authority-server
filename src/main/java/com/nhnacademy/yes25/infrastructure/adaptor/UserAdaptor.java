0f0af7748444c4e1973bf6cb31a53e15f16d0469
채상희 <chaesanghui142@gmail.com>
2024-06-25 14:44:56 +0900

    fix: "로그인 버그 수정"

--- src/main/java/com/nhnacademy/yes25/infrastructure/adaptor/UserAdaptor.java
+++ src/main/java/com/nhnacademy/yes25/infrastructure/adaptor/UserAdaptor.java
@@ -6,7 +6,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
-@FeignClient(name = "userAdaptor", url = "${eureka.gateway}/auth")
+@FeignClient(name = "userAdaptor", url = "${api.book-user}")
 public interface UserAdaptor {
 
     @PostMapping("/users")
