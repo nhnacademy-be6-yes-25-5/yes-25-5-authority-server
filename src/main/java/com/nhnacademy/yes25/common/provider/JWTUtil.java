bfee48690a9bc804e1aa316c5cca865935aea1a4
채상희 <chaesanghui142@gmail.com>
2024-06-25 13:38:16 +0900

    fix: "코드 정리 및 컨벤션 적용"

--- src/main/java/com/nhnacademy/yes25/common/provider/JWTUtil.java
+++ src/main/java/com/nhnacademy/yes25/common/provider/JWTUtil.java
@@ -1,9 +1,8 @@
 package com.nhnacademy.yes25.common.provider;
 
-import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.*;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
-
 import javax.crypto.SecretKey;
 import javax.crypto.spec.SecretKeySpec;
 import java.nio.charset.StandardCharsets;
@@ -19,8 +18,9 @@ import java.util.Date;
 public class JWTUtil {
 
     private final SecretKey secretKey;
+
     @Value("${jwt.expiration-ms}")
-    private static long JWT_EXPIRATION_MS; // 30분
+    private long JWT_EXPIRATION_MS; // 30분
 
     /**
      * 제공된 비밀 키를 사용하여 JWTUtil 인스턴스를 생성합니다.
@@ -29,36 +29,6 @@ public class JWTUtil {
      */
     public JWTUtil(@Value("${jwt.secret}") String jwtSecret) {
         secretKey = new SecretKeySpec(jwtSecret.getBytes(StandardCharsets.UTF_8), Jwts.SIG.HS256.key().build().getAlgorithm());
-    }
-
-    /**
-     * 제공된 JWT 토큰에서 사용자 이름을 추출합니다.
-     *
-     * @param token 사용자 이름을 추출할 JWT 토큰
-     * @return JWT 토큰에서 추출된 사용자 이름
-     */
-    public String getUsername(String token) {
-        return Jwts.parser().verifyWith(secretKey).build().parseSignedClaims(token).getPayload().get("username", String.class);
-    }
-
-    /**
-     * 제공된 JWT 토큰에서 사용자 역할을 추출합니다.
-     *
-     * @param token 사용자 역할을 추출할 JWT 토큰
-     * @return JWT 토큰에서 추출된 사용자 역할
-     */
-    public String getRole(String token) {
-        return Jwts.parser().verifyWith(secretKey).build().parseSignedClaims(token).getPayload().get("role", String.class);
-    }
-
-    /**
-     * 제공된 JWT 토큰이 만료되었는지 확인합니다.
-     *
-     * @param token 만료 여부를 확인할 JWT 토큰
-     * @return JWT 토큰이 만료된 경우 true, 그렇지 않은 경우 false
-     */
-    public Boolean isExpired(String token) {
-        return Jwts.parser().verifyWith(secretKey).build().parseSignedClaims(token).getPayload().getExpiration().before(new Date());
     }
 
     /**
@@ -71,12 +41,13 @@ public class JWTUtil {
      */
     public String createJwt(Long customerId, String userRole, String loginStatusName) {
         return Jwts.builder()
-                .claim("customerID", customerId)
+                .setHeaderParam("typ", "JWT")
+                .claim("customerId", customerId)
                 .claim("userRole", userRole)
                 .claim("loginStatusName", loginStatusName)
-                .issuedAt(new Date(System.currentTimeMillis()))
-                .expiration(new Date(System.currentTimeMillis() + JWT_EXPIRATION_MS))
-                .signWith(secretKey)
+                .setIssuedAt(new Date())
+                .setExpiration(new Date(System.currentTimeMillis() + JWT_EXPIRATION_MS))
+                .signWith(secretKey, SignatureAlgorithm.HS256)
                 .compact();
     }
 
