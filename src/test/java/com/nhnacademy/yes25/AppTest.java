56b7c08638645665d1f69c878501edf124e456c0
채상희 <chaesanghui142@gmail.com>
2024-07-05 11:09:56 +0900

    fix: "test: application-ci.yml 수정"

--- src/test/java/com/nhnacademy/yes25/AppTest.java
+++ src/test/java/com/nhnacademy/yes25/AppTest.java
@@ -1,13 +1,13 @@
-//package com.nhnacademy.yes25;
-//
-//import org.junit.jupiter.api.Test;
-//import org.springframework.boot.test.context.SpringBootTest;
-//
-//@SpringBootTest
-//class AppTest {
-//
-//    @Test
-//    void contextLoads() {
-//    }
-//
-//}
+package com.nhnacademy.yes25;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppTest {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
